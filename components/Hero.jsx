eb8a5beb395179d7a5ed22bdaceeffcd7f8ae887
Husban Baig <saifanees11@gmail.com>
2025-01-19 09:01:25 +0500

    first commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 import Autoplay from "embla-carousel-autoplay";
 import { TrustIndicators } from "./TrustIndicators";
 import Slider from "./Slider";
+import { motion } from "framer-motion";
 
 export default function Hero() {
   const { isActive, pageChanged, setPageChanged } = useNavbar();
@@ -21,24 +22,38 @@ export default function Hero() {
     Autoplay({ delay: 4000, stopOnInteraction: true })
   );
 
+  // Animation variants for sliding in from the top
+  const titleVariants = {
+    hidden: { opacity: 0, y: -20 }, // Start off-screen above
+    visible: { opacity: 1, y: 0 }, // End position
+  };
+
   return (
     <div
-      className={`lg:h-screen relative top-0 overflow-x-hidden bg-pencil text-background transition-all max-sm:h-[88vh] flex flex-col justify-between items-center gap-5 max-sm:gap-4 w-full lg:overflow-hidden ${
-        isActive ? "slide-out-top" : "slide-in-bottom"
-      } ${pageChanged ? "hidden" : ""}`}
+      className={`lg:h-screen relative top-0 overflow-x-hidden bg-pencil text-background transition-all max-sm:h-[88vh] flex flex-col justify-between items-center gap-5 max-sm:gap-4 w-full lg:overflow-hidden`}
     >
       <div className="fixed top-0 lg:text-5xl text-4xl w-full font-medium leading-none pb-24 h-full flex flex-col items-center justify-center">
         <div className="">
-          <h1
-            className={`leading-none lg:text-3xl text-2xl -mb-7 max-lg:-mb-5 font-clash font-medium flex break-words justify-start items-center text-left text-slide-in-bottom`}
+          <motion.h1
+            className={`leading-none lg:text-3xl text-2xl -mb-7 max-lg:-mb-5 font-clash font-medium flex break-words justify-start items-center text-left`}
+            initial="hidden"
+            animate="visible"
+            variants={titleVariants}
+            transition={{ duration: 0.5 }}
           >
             An All In One
-          </h1>
+          </motion.h1>
         </div>
         <div className="overflow-hidden">
-          <h1 className="font-yellow-tail pb-4 tracking-wide px-2 text-nowrap leading-none max-sm:text-center max-sm:mt-1 text-slide-in-bottom max-sm:break-words text-emerald-500">
+          <motion.h1
+            className="font-yellow-tail pb-4 tracking-wide px-2 text-nowrap leading-none max-sm:text-center max-sm:mt-1 max-sm:break-words text-emerald-500"
+            initial="hidden"
+            animate="visible"
+            variants={titleVariants}
+            transition={{ duration: 0.5 }}
+          >
             Visual Storyteller
-          </h1>
+          </motion.h1>
         </div>
         <p className="text-base text-background/90 text-center max-w-xl px-6 leading-normal">
           I&apos;m Saif, a designer and developer passionate about creativity.
